d56c620728a25179992cf7fc7d18197032006887
manikarora8368 <manikarora8368@gmail.com>
2022-07-03 22:03:33 +0530

    service workers updated

--- src/Pages/Outlets/Outlets.js
+++ src/Pages/Outlets/Outlets.js
@@ -2,9 +2,10 @@ import { useHistory, useLocation } from "react-router-dom";
 import OutletContainer from "./OutletContainer/OutletContainer";
 import "./Outlets.scss";
 import ArrowBackIcon from "@mui/icons-material/ArrowBack";
+import { Outlets as data } from "../../utils/data";
 const Outlets = () => {
   const location = useLocation();
-  let outlets = location?.state?.outlets;
+  let outlets = location?.state?.outlets ?? data;
   const history = useHistory();
   return (
     <div className="outlet__wrapper">
